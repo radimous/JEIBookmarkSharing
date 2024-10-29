370e5c8797b9c41dd343b241ec270958cc4dc3a2
radimous <jurda92@gmail.com>
2024-10-29 17:51:18 +0100

    add button for bookmark importing

---
+++ src/main/java/com/radimous/bookmarksharing/mixin/MixinBookmarkOverlay.java
@@ -0,0 +1,79 @@
+package com.radimous.bookmarksharing.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.radimous.bookmarksharing.BookmarkImportIcon;
+import com.radimous.bookmarksharing.Bookmarksharing;
+import mezz.jei.common.bookmarks.BookmarkList;
+import mezz.jei.common.gui.GuiScreenHelper;
+import mezz.jei.common.gui.TooltipRenderer;
+import mezz.jei.common.gui.elements.GuiIconButton;
+import mezz.jei.common.gui.overlay.IngredientGridWithNavigation;
+import mezz.jei.common.gui.overlay.bookmarks.BookmarkOverlay;
+import mezz.jei.common.gui.textures.Textures;
+import mezz.jei.common.input.IKeyBindings;
+import mezz.jei.common.input.IUserInputHandler;
+import mezz.jei.common.input.handlers.CombinedInputHandler;
+import mezz.jei.common.network.IConnectionToServer;
+import mezz.jei.common.util.ImmutableRect2i;
+import mezz.jei.core.config.IClientConfig;
+import mezz.jei.core.config.IWorldConfig;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Mixin(value = BookmarkOverlay.class, remap = false)
+public abstract class MixinBookmarkOverlay {
+    @Shadow private ImmutableRect2i parentArea;
+    @Unique private GuiIconButton bookmarkImportButton;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void init(BookmarkList bookmarkList, Textures textures, IngredientGridWithNavigation contents,
+                      IClientConfig clientConfig, IWorldConfig worldConfig, GuiScreenHelper guiScreenHelper,
+                      IConnectionToServer serverConnection, IKeyBindings keyBindings, CallbackInfo ci) {
+        this.bookmarkImportButton = new GuiIconButton(((BookmarkImportIcon)textures).getBookmarkImportIcon(), b -> Bookmarksharing.pasteBookmark(bookmarkList), textures);
+    }
+
+    @Inject(method = "drawScreen", at = @At("RETURN"))
+    private void drawScreen(Minecraft minecraft, PoseStack poseStack, int mouseX, int mouseY, float partialTicks,
+                            CallbackInfo ci) {
+        this.bookmarkImportButton.render(poseStack,mouseX, mouseY, partialTicks);
+    }
+
+    @Inject(method = "drawTooltips", at = @At("RETURN"))
+    private void drawTooltips(Minecraft minecraft, PoseStack poseStack, int mouseX, int mouseY, CallbackInfo ci) {
+        if (bookmarkImportButton.isMouseOver(mouseX, mouseY)) {
+            List<Component> tooltip = List.of(
+                new TextComponent("Import bookmark from clipboard"),
+                new TextComponent("Hover over item and press CTRL+C to copy it to clipboard.").withStyle(ChatFormatting.GRAY));
+            TooltipRenderer.drawHoveringText(poseStack, tooltip, mouseX, mouseY);
+        }
+    }
+
+    @Inject(method = "updateBounds(Ljava/util/Set;)Z", at = @At(value = "INVOKE", target = "Lmezz/jei/common/gui/elements/GuiIconToggleButton;updateBounds(Lmezz/jei/common/util/ImmutableRect2i;)V"), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void updateBounds(Set<ImmutableRect2i> guiExclusionAreas, CallbackInfoReturnable<Boolean> cir,
+                              ImmutableRect2i availableContentsArea, boolean contentsHasRoom,
+                              ImmutableRect2i contentsArea, ImmutableRect2i bookmarkButtonArea) {
+        // 4 pixel gap
+        ImmutableRect2i bookmarkImportButtonArea = this.parentArea.matchWidthAndX(contentsArea).keepBottom(20).keepLeft(20).moveRight(24);
+        this.bookmarkImportButton.updateBounds(bookmarkImportButtonArea);
+    }
+
+    @ModifyReturnValue(method = "createInputHandler", at = @At("RETURN"))
+    public IUserInputHandler createInputHandler(IUserInputHandler original) {
+        return new CombinedInputHandler(original, this.bookmarkImportButton.createInputHandler());
+    }
+}
